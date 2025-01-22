a526fd0249b135c780e180e26d6401c62f1d4bdb
민석 <47300870+seok019283501@users.noreply.github.com>
2025-01-22 17:30:01 +0900

    feat: 로그인 css 설정 (#1)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+import VisionCallProvider from './components/provider/VisionCallProvider';
 import SignIn from './components/auth/SignIn';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
+    <VisionCallProvider>
     <Router>
       <Routes>
       <Route path="/sign-in" element={<SignIn/>}/>
       </Routes>
     </Router>
+    </VisionCallProvider>
   );
 }
 
--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,12 +1,33 @@
 import { useEffect, useState, useContext, useCallback } from 'react';
 import axios from "axios";
+import '../../styles/SignIn.css'
 
 const SignIn = (props) =>{
 
+  
+
   return (
     <>
-      <div className="sign-in-container">
-        
+      <div className="sign-in-up-container">
+        <div className='sign-in-up-in-sub-container'>
+          <div className='sign-in-up-sub-title-container'>
+            <div className='sign-in-up-sub-title'>
+              로그인
+            </div>
+          </div>
+
+          <div className='sign-in-input-container'>
+            <input className='sign-in-input' type='text' placeholder='아이디를 입력해주세요.'/>
+            <input className='sign-in-input' type='password' placeholder='비밀번호를 입력해주세요.'/>
+            <input className='sign-in-up-button' type='button' value='로그인'/>
+          </div>
+
+          <div className='sign-in-url'>
+            <div className='sign-in-url-password'>비밀번호 찾기</div>
+            <div>/</div>
+            <div className='sign-in-url-sign-up'>회원가입</div>
+        </div>
+        </div>
       </div>
     </>
   );
