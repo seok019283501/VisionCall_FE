19c1cbefd3f817403fda38a9b3cc96d26f83d317
민석 <47300870+seok019283501@users.noreply.github.com>
2025-01-24 00:28:28 +0900

    feat: 사이드바 기본 디자인 완료 (#5)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import VisionCallProvider from './components/provider/VisionCallProvider';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
+import Sidebar from './components/sidbars/Sidebar';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css'
 function App() {
   return (
     <VisionCallProvider>
-    <Router>
-      <Routes>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
-      </Routes>
-    </Router>
+    <Sidebar>
+      <Router>
+        <Routes>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+        </Routes>
+      </Router>
+    </Sidebar>
     </VisionCallProvider>
   );
 }
---
+++ src/components/sidbars/Sidebar.js
@@ -0,0 +1,42 @@
+import { useEffect, useState, useContext, useCallback } from 'react';
+import axios from "axios";
+import '../../styles/Sidebar.css'
+
+const Sidebar = (props) =>{
+  const rest_api_url = process.env.REACT_APP_REST_API_URL;
+  const [signIn,setSignIn] = useState({
+    username:'',
+    password:''
+  })
+
+  // 로그인 실패 시 에러 메시지 상태
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleUsername = (e) => {
+    setSignIn({...signIn,username:e.target.value});
+  }
+
+  
+
+  return (
+    <>
+      <div className="sidebar-container">
+        <div className='sidebar-input-continaer'>
+          <div className='sidebar-search-container'>
+            <input className='sidebar-search' type='text' placeholder='검색'/>
+          </div>
+          <div className='sidebar-button-container'>
+            <input className='sidebar-button' type='button' value='방 목록'/>
+            <input className='sidebar-button' type='button' value='친구 목록'/>
+          </div>
+          <div className='sidebar-item-list-container'>
+
+          </div>
+          <input className='sidebar-setting-button' type='button' value='설정'/>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
